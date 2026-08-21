b07e9638fb477afdcf8190d32963092fe468e8eb
Lam Dao <lam.dao@edu.sait.ca>
2026-08-21 01:06:46 -0600

    remove all components

--- convex/_generated/api.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/**
- * Generated `api` utility.
- * Regenerate with: npx convex dev
- */
-import { anyApi } from "convex/server"
-
-export const api = anyApi
-export const internal = anyApi
--- convex/_generated/dataModel.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-/**
- * Generated data model types.
- * Regenerate with: npx convex dev
- */
-import type { DataModelFromSchemaDefinition } from "convex/server"
-import schema from "../schema.js"
-
-export type DataModel = DataModelFromSchemaDefinition<typeof schema>
